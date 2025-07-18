b737edeb772a5001e1a147fff50bbd4e1b46b353
paulbnc <paul23buannic@gmail.com>
2025-07-18 19:15:31 +0200

    launching

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,20 @@
 from pandas import read_csv, DataFrame
+import os
+from typing import Tuple, Optional
 
-def load_data() -> Tuple(DataFrame, DataFrame):
-    '''return Tuple(Dataframe of features, Dataframe of target)'''
-    return read_csv(r'/dataset/X.csv')
+ROOT_DIR = os.path.dirname(os.getcwd())
+
+def load_data() -> Optional[Tuple[DataFrame, DataFrame]]:
+    '''Return Tuple(DataFrame of features, DataFrame of target), or None if files are missing'''
+    x_path = os.path.join(ROOT_DIR, 'dataset', 'X.csv')
+    y_path = os.path.join(ROOT_DIR, 'dataset', 'y.csv')
+    
+    try:
+        x_df = read_csv(x_path)
+        y_df = read_csv(y_path)
+        return x_df, y_df
+    except FileNotFoundError:
+        print(f'Files not found. Expected at:\n- {x_path}\n- {y_path}')
+    except Exception as e:
+        print(f'An error occurred while loading data: {e}')
+    return None
